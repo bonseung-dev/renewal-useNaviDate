61ffe9e24e568cdb23fb48336b00659898618c2d
verdantgreeny <verdantgreeny@gmail.com>
2025-06-09 14:00:57 +0900

    Feat: 기념일 입력폼 및 리스트 ui 구현 #3

--- src/components/features/date-calendar/calendar-tabs.tsx
+++ src/components/features/date-calendar/calendar-tabs.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import DateTab from './date-tab';
-import AnniversaryTab from './anniversary-tab';
+import AnniversaryTab from './anniversary/anniversary-tab';
 
 const CalendarTabs = () => {
   const [activeTab, setActiveTab] = useState<'date' | 'anniversary'>('date');
@@ -10,15 +10,15 @@ const CalendarTabs = () => {
   return (
     <>
       {/* 탭 헤더 */}
-      <div className="flex justify-around border-b mb-4">
+      <div className="flex justify-around mb-4">
         <button
-          className={`py-2 px-4 ${activeTab === 'date' ? 'border-b-2 border-black font-bold' : ''}`}
+          className={`py-2 px-8 ${activeTab === 'date' ? 'border-b-2 border-black font-bold' : ''}`}
           onClick={() => setActiveTab('date')}
         >
           데이트
         </button>
         <button
-          className={`py-2 px-4 ${activeTab === 'anniversary' ? 'border-b-2 border-black font-bold' : ''}`}
+          className={`py-2 px-8 ${activeTab === 'anniversary' ? 'border-b-2 border-black font-bold' : ''}`}
           onClick={() => setActiveTab('anniversary')}
         >
           기념일
