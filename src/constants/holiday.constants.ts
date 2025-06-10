80021bb0e9d3c5fcf4b566fadb48e4ee83cc7f9e
verdantgreeny <verdantgreeny@gmail.com>
2025-06-10 12:32:42 +0900

    Refactor: holiday 관련 상수화 #7

---
+++ src/constants/holiday.constants.ts
@@ -0,0 +1,17 @@
+export const CALENDAR_ID = 'ko.south_korea#holiday@group.v.calendar.google.com';
+
+// 법정공휴일 목록
+export const LEGAL_HOLIDAYS = [
+  '새해첫날',
+  '설날',
+  '추석',
+  '삼일절',
+  '광복절',
+  '개천절',
+  '한글날',
+  '크리스마스',
+  '부처님오신날',
+  '어린이날',
+  '대체공휴일',
+  '대통령 선거',
+];
