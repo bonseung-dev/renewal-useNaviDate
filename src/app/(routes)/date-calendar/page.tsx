3bf2be755ce053c2c249bbd20d7b549f07cd1e80
verdantgreeny <verdantgreeny@gmail.com>
2025-06-13 20:51:30 +0900

    Style: 캘린더 페이지 레이아웃 수정 #9

--- src/app/(routes)/date-calendar/page.tsx
+++ src/app/(routes)/date-calendar/page.tsx
@@ -1,11 +1,7 @@
 import CalendarTabs from '@/components/features/date-calendar/calendar-tabs';
 
 const page = () => {
-  return (
-    <div className="w-full max-w-md mx-auto p-4">
-      <CalendarTabs />
-    </div>
-  );
+  return <CalendarTabs />;
 };
 
 export default page;
