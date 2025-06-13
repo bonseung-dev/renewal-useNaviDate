3f2dc6e80810b4a3b786d8ce42842ddbcbf0641c
verdantgreeny <verdantgreeny@gmail.com>
2025-06-13 20:50:44 +0900

    Fix: Image 컴포넌트 사용 #9

--- src/components/features/date-calendar/anniversary/anniversary-tab.tsx
+++ src/components/features/date-calendar/anniversary/anniversary-tab.tsx
@@ -5,6 +5,7 @@ import { Anniversary } from '@/types/anniversary.type';
 import { getAnniversaries } from '@/lib/hooks/use-anniversaries';
 import AnniversaryForm from './anniversary-form';
 import AnniversaryList from './anniversary-list';
+import Image from 'next/image';
 
 const AnniversaryTab = () => {
   const [anniversaries, setAnniversaries] = useState<Anniversary[]>([]);
@@ -26,16 +27,18 @@ const AnniversaryTab = () => {
       {/* 상단: 커플 정보 */}
       <div className="w-[320px] h-[52px] flex items-center justify-between bg-[#7BB4DD] rounded-[40px] px-4">
         <div className="flex items-center gap-2">
-          <img
+          <Image
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-Og0eTxPdFSH4GjaCkkbiqyeAjlLkafGbqA&s"
             alt="커플 이미지"
-            className="w-8 h-8 rounded-full object-cover"
+            width={32}
+            height={32}
+            className="h-auto w-8 rounded-full object-cover"
           />
           <span className="font-semibold text-sm text-white">짱구 ❤️ 수지</span>
         </div>
         <button
           onClick={handleAddClick}
-          className="text-xs bg-white/20 hover:bg-white/30 px-3 py-1 rounded-full text-white font-medium"
+          className="text-xs bg-[#7BB4DD] border border-white hover:bg-white hover:text-[#7BB4DD] px-3 py-1 rounded-full text-white font-medium"
         >
           기념일 추가
         </button>
