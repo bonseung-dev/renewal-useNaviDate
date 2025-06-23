b60c52a508b1ca515b0407935e7b61534e5af629
verdantgreeny <verdantgreeny@gmail.com>
2025-06-23 17:58:51 +0900

    Feat: nav 기능 구현 #10

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,10 +1,11 @@
+import Footer from '@/components/features/nav/footer';
+import Header from '@/components/features/nav/header';
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full h-screen flex flex-col items-center">
       {/* Header */}
-      <header className="w-full max-w-[360px] h-[52px] bg-white border-b border-gray-200 flex items-center justify-center z-10">
-        <span className="text-base font-semibold">임시헤더</span>
-      </header>
+      <Header />
 
       {/* 메인영역 */}
       <main className="flex-1 w-full max-w-[360px] overflow-y-auto px-4 py-3 bg-white">
@@ -12,9 +13,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       </main>
 
       {/* Footer */}
-      <footer className="w-full max-w-[320px] h-[52px] bg-white border border-gray-200 rounded-[20px] shadow-md mb-5 flex items-center justify-center z-10">
-        <span className="text-sm font-medium">임시푸터</span>
-      </footer>
+      <Footer />
     </div>
   );
 };
