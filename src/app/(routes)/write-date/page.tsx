5df923b5869302aa487ed2179a21f751b9529950
Jongyeon Kim <tyjysong0712@gmail.com>
2025-06-26 18:36:43 +0900

    Style: 제목 및 내용 입력 스타일링 #15

--- src/app/(routes)/write-date/page.tsx
+++ src/app/(routes)/write-date/page.tsx
@@ -27,17 +27,23 @@ const WriteDate = () => {
       {/* 구분선 */}
       <Divider />
 
-      <section>
-        {/* 제목 입력 */}
-        <input type="text" placeholder="제목을 입력해주세요." />
-      </section>
-
-      <section>
-        {/* 달력 */}
-        <button>달력</button>
-        {/* 내용 입력 */}
-        <div>
-          <input type="text" placeholder="내용을 입력해주세요." />
+      {/* 내용 입력 */}
+      <section className="w-full flex flex-col justify-center items-center gap-2 mt-2">
+        <div className="flex items-center justify-between w-full px-5 gap-1">
+          <button className="w-11 h-5 rounded-[50px] border border-skin7 text-xs font-extralight text-skin7">
+            달력
+          </button>
+          <input
+            type="text"
+            placeholder="제목을 입력해주세요."
+            className="w-full text-left placeholder:text-center placeholder:text-skin2 text-sm font-extralight"
+          />
+        </div>
+        <div className="w-full px-5">
+          <textarea
+            placeholder="내용을 입력해주세요."
+            className="w-full placeholder:text-center placeholder:text-skin2 text-sm font-extralight"
+          />
         </div>
       </section>
 
