ee405cbae1e7075863657e14529ed23d2e55bc6c
Jongyeon Kim <tyjysong0712@gmail.com>
2025-06-26 21:28:44 +0900

    Refactor: 게시글 등록 버튼 로직을 SubmitPostButton 컴포넌트로 분리 #15

--- src/components/features/write-date/submit-post.tsx
+++ src/components/features/write-date/submit-post.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import SubmitPostButton from './submit-post-button';
 
 const SubmitPost = () => {
   return (
     <div className="w-full px-5">
-      <button className="w-full h-9 bg-skin1 rounded-lg text-xs font-extralight text-skin5 ">
-        게시글 등록하기
-      </button>
+      <SubmitPostButton />
     </div>
   );
 };
