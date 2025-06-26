a023cb8b76365c4704d9f97f19ea59d6533cc8f0
Jongyeon Kim <tyjysong0712@gmail.com>
2025-06-26 23:09:39 +0900

    Style: 태그 입력 스타일링 #15

--- src/app/(routes)/write-date/page.tsx
+++ src/app/(routes)/write-date/page.tsx
@@ -36,11 +36,12 @@ const WriteDate = () => {
       <Divider />
 
       {/* 태그 입력 */}
-      <section>
-        <input type="text" placeholder="#태그" />
-        <input type="text" placeholder="#태그" />
-        <input type="text" placeholder="#태그" />
-        <input type="text" placeholder="#태그" />
+      <section className="my-3">
+        <input
+          type="text"
+          placeholder="#태그입력"
+          className="w-14 h-6 bg-skin2 rounded-[50px] text-[10px] text-center text-skin5 font-extralight placeholder:text-center placeholder:text-skin5"
+        />
       </section>
 
       {/* 게시글 등록 */}
