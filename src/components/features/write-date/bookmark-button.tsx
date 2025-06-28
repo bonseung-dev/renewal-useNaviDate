7f74fa0c8c9107871c84fa9f88bb839ee88c48d9
Jongyeon Kim <tyjysong0712@gmail.com>
2025-06-28 18:08:33 +0900

    Refactor: 북마크, 좋아요, 공유 버튼 각각 컴포넌트 분리 #22

---
+++ src/components/features/write-date/bookmark-button.tsx
@@ -0,0 +1,9 @@
+const BookmarkButton = () => {
+  return (
+    <button className="w-20 h-8 bg-skin7 rounded-lg text-white text-[10px] font-extralight">
+      북마크
+    </button>
+  );
+};
+
+export default BookmarkButton;
---
+++ src/components/features/write-date/share-button.tsx
@@ -0,0 +1,9 @@
+const ShareButton = () => {
+  return (
+    <button className="w-20 h-8 bg-skin1 rounded-lg text-white text-[10px] font-extralight">
+      공유
+    </button>
+  );
+};
+
+export default ShareButton;
