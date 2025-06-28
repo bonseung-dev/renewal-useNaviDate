b05898eaa2d1727d7da73e9412320345eba74f4f
Jongyeon Kim <tyjysong0712@gmail.com>
2025-06-28 17:53:48 +0900

    Style: 태그 출력 스타일링 #22

--- src/app/(routes)/date-detail/[id]/page.tsx
+++ src/app/(routes)/date-detail/[id]/page.tsx
@@ -65,11 +65,15 @@ const DateDetail = ({ params }: PageProps) => {
       <Divider />
 
       {/* 태그 출력 */}
-      <section>
-        <p>#태그1</p>
-        <p>#태그2</p>
-        <p>#태그3</p>
-        <p>#태그4</p>
+      <section className="flex flex-wrap justify-start items-center w-full py-3 px-5 gap-3">
+        {date.tags.map((tag, index) => (
+          <p
+            key={tag + index}
+            className="flex justify-center items-center w-fit h-6 p-1 rounded-[50px] border border-skin1 text-skin1 text-[10px] font-extralight whitespace-nowrap"
+          >
+            #{tag}
+          </p>
+        ))}
       </section>
 
       <section>
