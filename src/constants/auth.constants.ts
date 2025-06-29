ec5f6292e7fefe90a76cde6c4d612d63ed94d265
verdantgreeny <verdantgreeny@gmail.com>
2025-06-29 15:56:12 +0900

    Refactor: 로그인페이지 상수분리 #21

---
+++ src/constants/auth.constants.ts
@@ -0,0 +1,14 @@
+export const AUTH_ERRORS = {
+  SERVER_ERROR: '서버 오류가 발생했습니다.',
+  EMAIL_NOT_FOUND: '등록된 이메일이 없습니다.',
+  PASSWORD_MISMATCH: '비밀번호가 일치하지 않습니다.',
+  COOKIE_SET_FAILED: '쿠키 설정 실패',
+  LOGIN_FAILED: '로그인 처리 실패',
+};
+
+export const VALIDATION_MESSAGES = {
+  EMAIL_REQUIRED: '이메일을 입력해주세요',
+  EMAIL_INVALID: '유효한 이메일 형식이 아닙니다',
+  PASSWORD_REQUIRED: '비밀번호를 입력해주세요',
+  PASSWORD_MIN_LENGTH: '비밀번호는 최소 8자여야 합니다',
+};
