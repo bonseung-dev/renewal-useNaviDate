2debf5f7a5c9afcfbaee43c6b8566a3731910b19
verdantgreeny <verdantgreeny@gmail.com>
2025-07-01 16:36:23 +0900

    Cleanup: 주석 수정 #20

--- src/lib/mutations/bookmark.mutation.ts
+++ src/lib/mutations/bookmark.mutation.ts
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toggleBookmark } from '../services/community.services';
 import { QUERY_KEYS } from '@/constants/query-keys.constants';
 
-// 북마크 토글 뮤테이션 훅
+// 북마크 뮤테이션 훅
 export const useToggleBookmark = () => {
   const queryClient = useQueryClient();
   return useMutation({
