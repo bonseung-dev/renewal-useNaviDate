bcf35b9cf927777f1ea0dd5dec08384ca7d45ae7
Jongyeon Kim <tyjysong0712@gmail.com>
2025-07-06 19:45:08 +0900

    Feat: 태그 추가 로직 수정 및 입력값 처리 개선 #24

--- src/components/features/write-date/write-tag.tsx
+++ src/components/features/write-date/write-tag.tsx
@@ -1,30 +1,33 @@
 import Image from 'next/image';
 
 const WriteTag = ({
-  tags,
-  setTags,
+  tag,
+  setTag,
   inputValue,
   setInputValue,
 }: {
-  tags: string[];
-  setTags: (tags: string[]) => void;
+  tag: string[];
+  setTag: (tags: string[]) => void;
   inputValue: string;
   setInputValue: (inputValue: string) => void;
 }) => {
   const deleteTag = (index: number) => {
-    setTags(tags.filter((_, i) => i !== index));
+    setTag(tag.filter((_, i) => i !== index));
   };
 
   return (
     <section className="flex justify-between items-center gap-1 my-3">
-      {tags.map((tag, index) => (
-        <div key={index} className="flex justify-center items-center gap-1">
+      {tag.map((tag, index) => (
+        <div
+          key={tag + index}
+          className="flex justify-center items-center gap-1"
+        >
           <div className="flex items-center justify-center w-auto min-w-14 h-6 px-1 rounded-[50px] border border-skin1 text-skin1 text-[10px] font-extralight overflow-hidden">
             #{tag}
           </div>
           <button type="button" onClick={() => deleteTag(index)}>
             <Image
-              src="/cancel_icon.png"
+              src="/cancel-icon.png"
               alt="cancel icon"
               width={4}
               height={4}
@@ -39,7 +42,11 @@ const WriteTag = ({
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
-            setTags([...tags, inputValue]);
+            e.preventDefault();
+
+            if (!inputValue.trim()) return;
+
+            setTag([...tag, inputValue]);
             setInputValue('');
           }
         }}
