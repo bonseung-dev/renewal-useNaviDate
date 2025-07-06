268bda62102143ec8270fa6258909cc5444d5db9
Jongyeon Kim <tyjysong0712@gmail.com>
2025-07-06 19:46:57 +0900

    Feat: 게시물 공개/비공개 타입을 boolean에서 'private' | 'public'으로 변경 #24

--- src/components/features/write-date/post-visibility/post-visibility.tsx
+++ src/components/features/write-date/post-visibility/post-visibility.tsx
@@ -5,8 +5,8 @@ const PostVisibility = ({
   visibility,
   setVisibility,
 }: {
-  visibility: boolean;
-  setVisibility: (visibility: boolean) => void;
+  visibility: 'private' | 'public';
+  setVisibility: (visibility: 'private' | 'public') => void;
 }) => {
   return (
     <section className="flex justify-end items-center gap-[2px] mt-1 ml-auto mr-5">
