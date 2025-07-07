80f2a4c4955f3767dbfb632507c81ab6cef85efc
Jongyeon Kim <tyjysong0712@gmail.com>
2025-07-07 18:02:54 +0900

    Feat: 게시물 상세 페이지 경로 [dateId]에서 [postId]로 변경 및 데이터 fetching 로직 수정 및 추가 #34

---
+++ src/app/(routes)/date-detail/[postId]/page.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import UploadedImagesCarousel from '@/components/features/date-detail/uploaded-images/uploaded-images-carousel';
+import Divider from '@/components/features/write-date/divider';
+import PlaceName from '@/components/features/date-detail/place-name';
+import SearchedAddress from '@/components/features/date-detail/searched-address';
+import WrittenContent from '@/components/features/date-detail/written-content';
+import WrittenTags from '@/components/features/date-detail/written-tags';
+import { useQuery } from '@tanstack/react-query';
+import Buttons from '@/components/features/date-detail/buttons/buttons';
+import { QUERY_KEYS } from '@/constants/query-keys.constants';
+import { Post, PostImage, PostTag } from '@/types/post.type';
+import { BASE_URL } from '@/constants/url.constants';
+
+type PageProps = {
+  params: { postId: string };
+};
+
+const DateDetail = ({ params }: PageProps) => {
+  const { postId } = params;
+
+  const fetchPost = async () => {
+    const response = await fetch(`${BASE_URL}/posts/${postId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const fetchImages = async () => {
+    const response = await fetch(`${BASE_URL}/postImages?postId=${postId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const fetchTags = async () => {
+    const response = await fetch(`${BASE_URL}/postTags?postId=${postId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const {
+    data: post,
+    isPending,
+    isError,
+  } = useQuery<Post>({
+    queryKey: [QUERY_KEYS.WRITE_POSTS, postId],
+    queryFn: fetchPost,
+  });
+
+  const { data: images } = useQuery<PostImage[]>({
+    queryKey: [QUERY_KEYS.WRITE_IMAGES, postId],
+    queryFn: fetchImages,
+  });
+
+  const { data: tags } = useQuery<PostTag[]>({
+    queryKey: [QUERY_KEYS.WRITE_TAGS, postId],
+    queryFn: fetchTags,
+  });
+
+  if (isPending) return <div>로딩중...</div>;
+  if (isError) return <div>오류 발생!!!</div>;
+
+  return (
+    <form className="flex flex-col justify-center items-center px-1">
+      {/* 캐러셀 이미지 출력 */}
+      {images && <UploadedImagesCarousel images={images} />}
+
+      {/* 주소 출력 */}
+      {images && <SearchedAddress images={images} />}
+
+      {/* 구분선 */}
+      <Divider />
+
+      {/* 장소 이름 출력 */}
+      <PlaceName post={post} />
+
+      {/* 구분선 */}
+      <Divider />
+
+      {/* 게시글 제목 및 내용 출력 */}
+      <WrittenContent post={post} />
+
+      {/* 구분선 */}
+      <Divider />
+
+      {/* 태그 출력 */}
+      {tags && <WrittenTags tags={tags} />}
+
+      {/* 좋아요, 북마크, 공유 버튼 */}
+      <Buttons />
+    </form>
+  );
+};
+
+export default DateDetail;
