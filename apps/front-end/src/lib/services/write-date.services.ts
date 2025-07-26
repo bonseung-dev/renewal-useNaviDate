5aa9bf2e69b5cae0b3a1219de18c06ede5baa069
Jongyeon <tyjysong0712@gmail.com>
2025-07-26 21:09:10 +0900

    Refactor: API 호출 로직을 fetchWithAuth로 변경 및 엔드포인트 수정 #44

--- apps/front-end/src/lib/services/write-date.services.ts
+++ apps/front-end/src/lib/services/write-date.services.ts
@@ -1,35 +1,27 @@
 import { BASE_URL } from '@/constants/url.constants';
+import { fetchWithAuth } from '@/lib/utils/api';
 import { Post, PostImage, PostTag } from '@use-navi-date/shared';
 
 // 포스트 게시
 export const createPost = async (newPost: Post) => {
-  await fetch(`${BASE_URL}/posts`, {
+  await fetchWithAuth(`${BASE_URL}/api/posts`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(newPost),
   });
 };
 
 // 이미지 업로드
 export const createImage = async (newImage: PostImage) => {
-  await fetch(`${BASE_URL}/postImages`, {
+  await fetchWithAuth(`${BASE_URL}/api/images`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(newImage),
   });
 };
 
 // 태그 입력
 export const createTag = async (newTag: PostTag) => {
-  await fetch(`${BASE_URL}/postTags`, {
+  await fetchWithAuth(`${BASE_URL}/postTags`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(newTag),
   });
 };
