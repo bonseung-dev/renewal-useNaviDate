2146f87756e1ba1451b7872a2c26a625fc772629
verdantgreeny <verdantgreeny@gmail.com>
2025-08-17 23:40:14 +0900

    Chore: my-post-tab 주석 추가 #55

--- apps/front-end/src/components/features/my-page/my-post-tab.tsx
+++ apps/front-end/src/components/features/my-page/my-post-tab.tsx
@@ -8,6 +8,10 @@ type MyPostTabProps = {
 };
 
 const MyPostTab = ({ userId }: MyPostTabProps) => {
+  // 현재 useCommunityData를 사용해서 전체 포스트를 가져오고 있습니다.
+  // 원래는 `/posts/my` 엔드포인트를 통해 내 포스트만 가져오고 싶었으나,
+  // posts.controller.ts를 확인해보니 `my` 엔드포인트가 존재하지 않는거 같습니다!
+  // 그래서 일단 전체 포스트를 가져온 후, userId로 필터링하여 내 포스트를 가져오고 있습니다!
   const { posts, users, likes, bookmarks, isLoading } = useCommunityData('');
 
   // 내가 작성한 포스트만 필터링
