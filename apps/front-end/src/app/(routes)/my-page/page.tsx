0b8c5f2e5d39863728962935601516e26e06af51
verdantgreeny <verdantgreeny@gmail.com>
2025-08-19 13:34:03 +0900

    Fix: 마이페이지 및 설정페이지 메타데이터 추가 #55

--- apps/front-end/src/app/(routes)/my-page/page.tsx
+++ apps/front-end/src/app/(routes)/my-page/page.tsx
@@ -2,6 +2,20 @@ import LoginPrompt from '@/components/features/date-calendar/login-prompt';
 import MyPageTabs from '@/components/features/my-page/my-page-tabs';
 import MyProfileHeader from '@/components/features/my-page/my-profile-header';
 import { getServerCookie, getUserIdFromToken } from '@/lib/utils/cookes.utils';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '마이페이지 || useNavidate( )',
+  description: '회원님의 정보 및 활동 내역을 확인할 수 있습니다',
+  robots: {
+    index: false,
+  },
+  openGraph: {
+    title: 'useNavidate( ) - 마이페이지',
+    description: '내 정보와 활동을 확인하세요',
+    images: '/navidate-logo_blue.png',
+  },
+};
 
 const page = async () => {
   const token = getServerCookie('access_token');
--- apps/front-end/src/app/(routes)/settings/page.tsx
+++ apps/front-end/src/app/(routes)/settings/page.tsx
@@ -2,44 +2,66 @@ import LogoutButton from '@/components/features/nav/log-out-test';
 import SettingsSwitches from '@/components/features/settings/settings-swtiches';
 import { getServerCookie } from '@/lib/utils/cookes.utils';
 import { ChevronRight, Store } from 'lucide-react';
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: '환경설정 || useNavidate( )',
+  description: '알림 / 테마 등을 설정할 수 있습니다',
+  robots: {
+    index: false,
+  },
+  openGraph: {
+    title: 'useNavidate( ) - 환경설정',
+    description: '알림 및 테마 등 환경설정을 변경하세요',
+    images: '/navidate-logo_blue.png',
+  },
+};
 
 const Page = () => {
   const isLoggedIn = !!getServerCookie('access_token');
   return (
     <div className="w-[320px] pt-[12px] h-[454px]">
       {/* Header */}
-      <div className="flex items-center justify-between px-[20px]">
+      <header className="flex items-center justify-between px-[20px]">
         <h1 className="text-b-h2 font-bold text-skin1">환경설정</h1>
         <LogoutButton isLoggedIn={isLoggedIn} />
-      </div>
+      </header>
 
       {/* 구분선 */}
       <div className="w-[320px] h-[1px] bg-skin3 mt-[20px]" />
 
-      {/* 세팅 옵션*/}
+      {/* 세팅 옵션 */}
       <SettingsSwitches />
 
       {/* 구분선 */}
       <div className="w-[320px] h-[1px] bg-skin3 mt-[20px]" />
 
-      {/* 상점 페이지 버튼 */}
-      <button className="w-[320px] h-[52px] mt-[20px] rounded-full bg-skin1 text-skin5 flex items-center justify-center gap-2 hover:bg-skin6 hover:text-font3">
+      {/* 상점 링크 */}
+      <Link
+        href="/settings" //임시로
+        className=" w-[320px] h-[52px] mt-[20px] rounded-full bg-skin1 text-skin5 flex items-center justify-center gap-2 hover:bg-skin6 hover:text-font3"
+      >
         <Store size={18} />
         <span className="text-b-h1 font-bold">상점 페이지</span>
-      </button>
+      </Link>
 
-      {/* Links */}
-      <div className="flex flex-col mt-[20px] gap-[16px] px-[20px]">
-        {['약관정책', '공지사항', '튜토리얼', '개발자 소개'].map((text) => (
-          <button
-            key={text}
-            className="flex items-center justify-between text-m-h3 text-font2 hover:text-skin1 hover:font-bold"
-          >
-            <span>{text}</span>
-            <ChevronRight size={14} />
-          </button>
-        ))}
-      </div>
+      {/* 링크 목록 */}
+      <nav className="mt-[20px] px-[20px]">
+        <ul className="flex flex-col gap-[16px]">
+          {['약관정책', '공지사항', '튜토리얼', '개발자 소개'].map((text) => (
+            <li key={text}>
+              <Link
+                href="/settings" //임시로
+                className="flex items-center justify-between text-m-h3 text-font2 hover:text-skin1 hover:font-bold"
+              >
+                <span>{text}</span>
+                <ChevronRight size={14} />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 };
