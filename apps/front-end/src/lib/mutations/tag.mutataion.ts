c6ad973d38554c0f24765da597810221adee84f0
Jongyeon <tyjysong0712@gmail.com>
2025-08-24 16:15:26 +0900

    Refactor: 쿼리키를 상수화된 쿼리키로 변경 #51

--- apps/front-end/src/lib/mutations/tag.mutataion.ts
+++ apps/front-end/src/lib/mutations/tag.mutataion.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { TagService } from '../api/services';
 import { getClientAuthToken } from '../utils/api';
 import { Tag } from '@use-navi-date/shared';
+import { QUERY_KEYS } from '@/constants/query-keys.constants';
 
 const useCreateTagMutation = () => {
   const queryClient = useQueryClient();
@@ -12,13 +13,13 @@ const useCreateTagMutation = () => {
 
       const response = await TagService.addTag(tag, token);
       if (!response.success) {
-        throw new Error(response.message || '이미지 업로드에 실패했습니다.');
+        throw new Error(response.message || '태그 생성에 실패했습니다.');
       }
 
       return response.data;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tags'] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.WRITE_TAGS] });
     },
   });
 };
@@ -38,7 +39,7 @@ export const useDeleteTagMutation = () => {
       return response.data;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tags'] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.WRITE_TAGS] });
     },
   });
 };
