67f8f7669abe4d7260bbea10138b9b0c8742b103
Jongyeon <tyjysong0712@gmail.com>
2025-08-27 22:55:03 +0900

    Feat: 달력 버튼에 type을 button으로 부여 #51

--- apps/front-end/src/components/features/write-date/write-content.tsx
+++ apps/front-end/src/components/features/write-date/write-content.tsx
@@ -1,4 +1,4 @@
-import { Post } from "@use-navi-date/shared";
+import { Post } from '@use-navi-date/shared';
 
 const WriteContent = ({
   content,
@@ -14,7 +14,10 @@ const WriteContent = ({
   return (
     <section className="w-full flex flex-col justify-center items-center gap-2 mt-2">
       <div className="flex items-center justify-between w-full px-5 gap-1">
-        <button className="w-11 h-5 rounded-[50px] border border-skin7 text-xs font-extralight text-skin7">
+        <button
+          type="button"
+          className="w-11 h-5 rounded-[50px] border border-skin7 text-xs font-extralight text-skin7"
+        >
           달력
         </button>
         <input
