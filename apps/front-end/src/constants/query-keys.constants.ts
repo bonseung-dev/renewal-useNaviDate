d49c87fd9250e46566779843ed7c1e743c86f862
Jongyeon <tyjysong0712@gmail.com>
2025-08-29 13:43:57 +0900

    Refactor: 쿼리키 value값 수정 #51

--- apps/front-end/src/constants/query-keys.constants.ts
+++ apps/front-end/src/constants/query-keys.constants.ts
@@ -7,9 +7,9 @@ export const QUERY_KEYS = {
   IMAGES: 'community-post-images',
   LIKES: 'community-likes',
   BOOKMARKS: 'community-bookmarks',
-  WRITE_POSTS: 'write-posts',
-  WRITE_IMAGES: 'write-post-images',
-  WRITE_TAGS: 'write-post-tags',
+  WRITE_POSTS: 'posts',
+  WRITE_IMAGES: 'images',
+  WRITE_TAGS: 'tags',
   ANNIVERSARIES: 'anniversaries',
   ANNIVERSARIES_BY_COUPLE: (coupleId: number) => ['anniversaries', coupleId],
 } as const;
